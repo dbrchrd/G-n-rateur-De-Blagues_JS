27378bc04efc75b5643ae9fed27feef9b6ee9034
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-10-29 19:32:02 +0200

    Update translations.js, [EN] language added

---
+++ translations.js
@@ -0,0 +1,14 @@
+export const tltn = {
+  en: { ////////// EN ////////// EN ////////// EN ////////// EN ////////// EN ////////// EN
+    "params-h2": "Parameters",
+    "select-language-label": "Select a language:",
+    "blacklist-flags-inputs-label": "Select flags to blacklist:",
+    "no-matching-error": "Sorry, no matching joke found.",
+    "blacklist-nsfw-label" : "nsfw",
+    "blacklist-religious-label" : "religious",
+    "blacklist-political-label" : "political",
+    "blacklist-racist-label" : "racist",
+    "blacklist-sexist-label" : "sexist",
+    "blacklist-explicit-label" : "explicit"
+  }
+};
